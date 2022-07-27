68e705d2f3fd05218b5e984f1dba1323603fd9ec
SaifHayek <seefhayek4@gmail.com>
2022-07-27 22:36:05 +0300

    make different modifing in js funcitons related #1

--- js/dom/products.js
+++ js/dom/products.js
@@ -1,17 +1,16 @@
 const products =  getItem('allProduct') 
-console.log(products)
 const tableBody = document.querySelector('.table-body')
 
 function addToTable (obj,index) {
     tableBody.innerHTML += `
 <tr>
-    <td>${index}</td>
+    <td>${index + 1 }</td>
     <td>${obj.name}</td>
     <td>${obj.price}</td>
     <td>${obj.category}</td>
     <td><img src="${obj.img}" alt="random"></td>
     <td class="td-btn"><button class="edit-btn table-btn" onClick = "editItem(${obj.id})">Edit</button></td>
-    <td class="td-btn"><button class="delete-btn table-btn" onClick = "deleteItem(${obj.id}")>Delete</button></td>
+    <td class="td-btn"><button class="delete-btn table-btn" onClick = "deleteItem(${obj.id})">Delete</button></td>
 </tr>
     `
 }
@@ -20,17 +19,13 @@ function editItem(id){
     window.location = "./editProduct.html"
 }
 function deleteItem(id){
-    removeItemFrom('allProducts',id)
+    removeItemFrom('allProduct',id)
     tableBody.innerHTML = ''
-    products.forEach((ele,index) => {
-        console.log(ele);
+    getItem('allProduct').forEach((ele,index) => {
         addToTable(ele,index)
     })
-
-
 }
 products.forEach((ele,index) => {
-    console.log(ele);
     addToTable(ele,index)
 })
 
